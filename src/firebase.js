2e9bc0b96e0c4778a8e7a468f96918af1dfbe7ba
Your Name <youremail@example.com>
2023-10-30 06:22:21 +0100

    added search  button

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCHPay3qB2gbdUhHjlr3j9FlRqoenBZms8",
+    authDomain: "e-challenge-14f34.firebaseapp.com",
+    projectId: "e-challenge-14f34",
+    storageBucket: "e-challenge-14f34.appspot.com",
+    messagingSenderId: "604266098613",
+    appId: "1:604266098613:web:3c7e2a5444b23267d7b3fc",
+    measurementId: "G-026CS2NVZ1"
+  };
